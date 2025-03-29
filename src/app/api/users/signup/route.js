4bf5917219bf3ac55c4aad1533fb02150230f2c4
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2025-03-29 07:02:29 +0000

    new routes file create

--- src/app/api/users/signup/route.js
+++ src/app/api/users/signup/route.js
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import User from '@/models/userModel';
 import connectDB from '@/dbConfig/dbConfig';
 import sendEmail from '@/helpers/mailer';
-import crypto from 'crypto';
 
 export async function POST(req) {
   try {
@@ -22,34 +21,39 @@ export async function POST(req) {
       );
     }
 
-    // Create a verification token
-    const verifyToken = crypto.randomBytes(32).toString('hex');
-    const verifyTokenExpiry = Date.now() + 3600000; // Token valid for 1 hour
+    // Declare newUser outside the try block
+    let newUser;
 
     // Create a new user
-    const newUser = await User.create({
-      username,
-      email,
-      password,
-      verifyToken,
-      verifyTokenExpiry,
-    });
+    try {
+      newUser = new User({
+        username,
+        email,
+        password,
+      });
+
+      const savedUser = await newUser.save();
+      console.log('User created:', savedUser);
+    } catch (error) {
+      console.error('Failed to create user:', error);
+      return NextResponse.json({ error: 'Failed to create user.' }, { status: 500 });
+    }
 
     // Send verification email
     await sendEmail(
       email,
       'verification',
-      newUser._id
-    ); // here we may need to receive object
+      newUser._id // Now accessible because newUser is declared outside the inner try block
+    );
 
     return NextResponse.json(
-      { message: 'User registed successfully. Verification email sent.' },
+      { message: 'User registered successfully. Verification email sent.' },
       { status: 201 }
     );
   } catch (error) {
     console.error('Error in signup route:', error.message);
     return NextResponse.json(
-      { error: 'Something went wrong. Please try again later.' },
+      { error: 'Something went wrong. Error in signup.' },
       { status: 500 }
     );
   }
